f2cd8bf32f1a18c4c64de7ad9e1381202e4c7e11
AmmonMatthews <ammonmatthews@gmail.com>
2020-08-03 15:41:54 -0600

    assignment

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,37 @@
-# Your code here
+from operator import itemgetter, attrgetter
 
+with open("robin.txt") as f:
+    words = f.read()
+
+# print(words)
+
+def word_count(s):
+    # Your code here
+    counts = {}
+    punctuations =  ' : ; , . - + = / \ | [ ] { } ( ) * ^ & " '
+    
+    
+    for x in s: 
+        if x in punctuations:
+            s = s.replace(x, " ")
+
+    s = " ".join(s.split())
+    
+    new_s = s.split()    
+
+    for c in new_s:
+        c = c.lower()  # case insensitive
+        if c in counts:
+            counts[c] += '#'
+        else:
+            counts[c] = '#'
+
+    items = list(counts.items())
+    # items.sort(key=lambda e: e[1], reverse= True)
+    # items.sort()
+    items = sorted(items, key=itemgetter(1), reverse=True)
+    # new_items.sort()
+    # items.sorted(key= itemgetter(1, -2), reverse=True)
+    return items
+
+print(word_count(words))
--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -6,6 +6,47 @@ with open("input.txt") as f:
 
 # TODO: analyze which words can follow other words
 # Your code here
+l = words.split()
+# print(l)
+d = {}
+f = {}
+g = []
+x = 0
+while x < len(l) - 1:
+    # # print(g.append(l[x+1]))
+    # word = l[x+1]
+    # key = d[l[x]]
+    # print(g)
+    # if key in d:
+    #     key = g.append(word)
+    # else: 
+    #     key = g
+    
+    # print(g)
+
+    # d[l[x]] = g.append(word)
+    if l[x] in d:
+        f[l[x]] += 1
+        d[l[x]] += " " + (l[x + 1])
+    else:
+        f[l[x]] = 1
+        d[l[x]] = l[x + 1]
+    x += 1 
+
+key = d.keys()
+v = d.values()
+# v.split()
+print(key)
+
+    
+# items = list(d.items())
+# items.sort()
+# print(items)
+    # print(x)
+    # print(l[x])    
+# print(d)
+# print(f)
+    
 
 
 # TODO: construct 5 random sentences
